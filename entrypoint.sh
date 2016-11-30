49ddfdab78395360c8094d7e75ab049c18e292fb
Sven Speckmaier <sps@ipunkt.biz>
2016-11-30 09:31:35 +0100

    removed node_modules symlinking
    node_modules as symlink causes errors during build

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,13 @@ export HOME=/home/user
 mkdir -p /data/npm/workspace/node_modules
 chmod 777 /data/npm/workspace/node_modules
 
-if [ ! -L "node_modules" ] ; then
-	ln -s /data/npm/workspace/node_modules
+# This is ment to hide the node_modules directory inside a volume within docker
+# It is disabled because the build process errors with aSource sourcemap not found.
+# if [ ! -L "node_modules" ] ; then
+#	ln -s /data/npm/workspace/node_modules
+#fi
+if [ -L "node_modules" ] ; then
+    rm node_modules
 fi
 
 exec /usr/local/bin/gosu user "$@"
